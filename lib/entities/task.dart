b32c87bc90aa0d7b01cc7ef61b0072fc3b40a1ed
Nerphist <nikolailgv@ukr.net>
2021-03-15 12:34:15 +0200

    task entity added

---
+++ lib/entities/task.dart
@@ -0,0 +1,15 @@
+
+class Task{
+  int priority;
+  String name;
+  String description;
+  String status;
+  DateTime dueDate;
+  DateTime completionDate;
+  double grade;
+
+  Task(this.priority, this.name, this.description, this.status, this.dueDate,
+      this.completionDate, this.grade);
+
+
+}
