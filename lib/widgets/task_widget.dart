18c0f566a2514d0142d1c46b88bb1eba19bf9a8a
Nerphist <nikolailgv@ukr.net>
2021-03-15 16:01:24 +0200

    task list changes

--- lib/widgets/task_widget.dart
+++ lib/widgets/task_widget.dart
@@ -1,4 +1,6 @@
+import 'package:data/constants.dart';
 import 'package:data/entities/task.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class TaskWidget extends StatelessWidget {
@@ -8,6 +10,38 @@ class TaskWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(child: Text(this.task.name));
+    // return ListTile(
+    //     title: Padding(
+    //         padding: EdgeInsets.all(20),
+    //         child: Container(
+    //           child: Text(this.task.name),
+    //         )));
+    return GestureDetector(
+        onTap: () {},
+        child: Card(
+            margin: EdgeInsets.only(left: 16, top: 16, right: 16),
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(8))),
+            child: Padding(
+              padding: EdgeInsets.all(16),
+              child: Column(children: [
+                Row(children: [
+                  Text(this.task.name,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 17.0,
+                      )),
+                ]),
+                SizedBox(height: 5),
+                Container(
+                  child: Text(this.task.description),
+                  decoration: BoxDecoration(
+                      color: Colors_.primaryLightest,
+                      borderRadius: BorderRadius.all(Radius.circular(8))),
+                  padding: EdgeInsets.all(16),
+                  width: 128 * 3.0,
+                )
+              ]),
+            )));
   }
 }
